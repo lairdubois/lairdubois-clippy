72ba1965283dc01a324855ac80428cb82419e833
Boris Beaulant <bo@borisbeaulant.com>
2023-10-23 10:33:52 +0200

    ~ dllmain improvements

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,19 +1,37 @@
-#include <windows.h>
-#include <clippy.h>
+// Docs : https://learn.microsoft.com/fr-fr/windows/win32/dlls/dllmain
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+#include <windows.h>
+
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,   // handle to DLL module
+    DWORD fdwReason,      // reason for calling function
+    LPVOID lpvReserved )  // reserved
 {
-  switch (ul_reason_for_call)
+  // Perform actions based on the reason for calling.
+  switch( fdwReason )
   {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-  }
-  return TRUE;
-}
+      // Initialize once for each new process.
+      // Return FALSE to fail DLL load.
+      break;
 
+    case DLL_THREAD_ATTACH:
+      // Do thread-specific initialization.
+      break;
+
+    case DLL_THREAD_DETACH:
+      // Do thread-specific cleanup.
+      break;
+
+    case DLL_PROCESS_DETACH:
+
+      if (lpvReserved != nullptr)
+      {
+          break; // do not do cleanup if process termination scenario
+      }
+
+      // Perform any necessary cleanup.
+      break;
+  }
+  return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
