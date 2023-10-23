70c2349b892ace30a716dd03d499ea8941915e70
mobilarte <martin@maxsyma.com>
2023-10-23 18:01:56 +0200

    ~ VisualStudio

--- include/clippy.h
+++ include/clippy.h
@@ -1,23 +1,32 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef LIBCLIPPY_EXPORTS
+#define LIBCLIPPY_API __declspec(dllexport)
+#else
+#define LIBCLIPPY_API __declspec(dllimport)
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void c_clear_subjects(void);
-void c_append_subject(const int64_t *coords, size_t len);
+LIBCLIPPY_API void c_clear_subjects(void);
+LIBCLIPPY_API void c_append_subject(const int64_t *coords, size_t len);
 
-void c_clear_clips(void);
-void c_append_clip(const int64_t *coords, size_t len);
+LIBCLIPPY_API void c_clear_clips(void);
+LIBCLIPPY_API void c_append_clip(const int64_t *coords, size_t len);
 
-size_t c_compute_union(void);
-size_t c_compute_difference(void);
+LIBCLIPPY_API size_t c_compute_union(void);
+LIBCLIPPY_API size_t c_compute_difference(void);
 
-void c_clear_solution(void);
-size_t c_get_solution_len();
-size_t c_get_solution_path_len_at(int index);
-int64_t* c_get_solution_path_coords_at(int index);
+LIBCLIPPY_API void c_clear_solution(void);
+LIBCLIPPY_API size_t c_get_solution_len();
+LIBCLIPPY_API size_t c_get_solution_path_len_at(int index);
+LIBCLIPPY_API int64_t* c_get_solution_path_coords_at(int index);
+
+LIBCLIPPY_API void c_free_coords(const int64_t* coords);
+
 
 #ifdef __cplusplus
 }
--- src/clippy.cpp
+++ src/clippy.cpp
@@ -1,5 +1,7 @@
 #include "clipper2/clipper.core.h"
 #include "clipper2/clipper.h"
+
+
 #include <clippy.h>
 
 using namespace Clipper2Lib;
@@ -10,11 +12,11 @@ extern "C" {
 
 Paths64 subjects, clips, solution;
 
-void c_clear_subjects(void) {
+LIBCLIPPY_API void c_clear_subjects(void) {
   subjects.clear();
 }
 
-void c_append_subject(const int64_t *coords, size_t len) {
+LIBCLIPPY_API void c_append_subject(const int64_t *coords, size_t len) {
 
   std::vector<int64_t> list;
   list.reserve(len);
@@ -23,11 +25,11 @@ void c_append_subject(const int64_t *coords, size_t len) {
   subjects.push_back(MakePath(list));
 }
 
-void c_clear_clips(void) {
+LIBCLIPPY_API void c_clear_clips(void) {
   clips.clear();
 }
 
-void c_append_clip(const int64_t *coords, size_t len) {
+LIBCLIPPY_API void c_append_clip(const int64_t *coords, size_t len) {
 
   std::vector<int64_t> list;
   list.reserve(len);
@@ -36,29 +38,29 @@ void c_append_clip(const int64_t *coords, size_t len) {
   clips.push_back(MakePath(list));
 }
 
-size_t c_compute_union(void) {
+LIBCLIPPY_API size_t c_compute_union(void) {
   solution = Union(subjects, clips, FillRule::NonZero);
   return solution.size();
 }
 
-size_t c_compute_difference(void) {
+LIBCLIPPY_API size_t c_compute_difference(void) {
   solution = Difference(subjects, clips, FillRule::NonZero);
   return solution.size();
 }
 
-void c_clear_solution(void) {
+LIBCLIPPY_API void c_clear_solution(void) {
   solution.clear();
 }
 
-size_t c_get_solution_len() {
+LIBCLIPPY_API size_t c_get_solution_len() {
   return solution.size();
 }
 
-size_t c_get_solution_path_len_at(int index) {
+LIBCLIPPY_API size_t c_get_solution_path_len_at(int index) {
   return solution[index].size();
 }
 
-int64_t* c_get_solution_path_coords_at(int index) {
+LIBCLIPPY_API int64_t* c_get_solution_path_coords_at(int index) {
 
   Path64 path = solution[index];
 
@@ -72,6 +74,11 @@ int64_t* c_get_solution_path_coords_at(int index) {
 
   return coords;
 }
+
+LIBCLIPPY_API void c_free_coords(const int64_t* coords) {
+    free((void *) coords);
+}
+
 
 #ifdef __cplusplus
 }
